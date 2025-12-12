f996e7b25f2fe0fdad3c3ba342f827cef1868809
wplct <wplct@qq.com>
2025-12-12 18:45:30 +0800

    feat(form): 添加输入框组件支持
    - 在表单构建器中引入 InputText 组件
    - 更新 __init__.py 文件以导出新组件
    - 支持文本输入类型的表单项配置

--- src/amis_python/builder/form/__init__.py
+++ src/amis_python/builder/form/__init__.py
@@ -1,2 +1,3 @@
 from .form import Form, FormMode, LabelAlign, SubmitTextAlign, InitDataTarget, FormMessages, FormHorizontal, FormRedirect, FormReload, FormDebugConfig
 from .formitem import FormItem
+from .input_text import InputText
